011678da54cd3bbb9f175632ffe725f0db42d6ff
Jeison Fabian Sandoval Castaneda <jeison.sandoval@cadena.com.co>
2021-04-23 21:10:19 -0500

    inicio ok

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     
   },
   output: {
-    path: path.resolve(__dirname, './public'),
+    path: path.join(__dirname, './public'),
     filename: '[name].bundle.js'
   },
   devtool: 'inline-source-map',
